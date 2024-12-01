73c064f2b13bc3a626cc769eb499c3af7deb4b38
EmilyWProjects <Emily.Warford@keyin.com>
2024-12-01 00:02:28 -0330

    Added functionality to index, login, and signup pages

--- index.js
+++ index.js
@@ -41,26 +41,62 @@ const USERS = [
 
 // GET /login - Render login form
 app.get("/login", (request, response) => {
+    //Load page
     response.render("login");
 });
 
+
 // POST /login - Allows a user to login
 app.post("/login", (request, response) => {
-
+    //Validate form input
+    const { email, password } = request.body;
+    if (!email || !password) {
+      return response.render("login", { error: "Email or password cannot be blank!" });
+    }
+    //Check if user exists
+    const user = USERS.find((user) => user.email === email);
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+        return response.render("login", { error: "Password or email is invalid!" });
+    }
+    //Stores session 
+    request.session.user = user;
+    response.redirect("/landing");
 });
 
 // GET /signup - Render signup form
 app.get("/signup", (request, response) => {
+    //Load page
     response.render("signup");
 });
 
+
 // POST /signup - Allows a user to signup
 app.post("/signup", (request, response) => {
-    
+    //Validate form input
+    const { email, username, password } = request.body;
+    if (!email || !username || !password) {
+      return response.render("signup", { error: "Username, email, or password cannot be blank!" });
+    }
+    if (USERS.find((user) => user.email === email)) {
+        return response.render("signup", { error: "Email already in use!" });
+    }
+    if (USERS.find((user) => user.username === username)) {
+        return response.render("signup", { error: "Username already in use!" });
+    }
+    //Add user to array
+    USERS.push({
+        id: USERS.length +1, 
+        email, 
+        username, 
+        password: bcrypt.hashSync(password, SALT_ROUNDS), 
+        role: "regular"
+    });
+    return response.redirect('/login');
 });
 
 // GET / - Render index page or redirect to landing if logged in
 app.get("/", (request, response) => {
+    //Load pages
     if (request.session.user) {
         return response.redirect("/landing");
     }
